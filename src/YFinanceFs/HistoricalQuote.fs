9d3b65aeee83d10a343b7258582f935f8e4e400c
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-08-31 09:37:24 +0200

    Added Json deserialization machinery

--- src/YFinanceFs/HistoricalQuote.fs
+++ src/YFinanceFs/HistoricalQuote.fs
@@ -1,8 +1,11 @@
 module Network.Yahoo.HistoricalQuote
 
+open Chiron
+open Chiron.Operators
+
 type HistoricalQuote =
     {
-        symbol         : string
+         symbol        : string
        ; date          : string
        ; opening       : string
        ; high          : string
@@ -10,4 +13,24 @@ type HistoricalQuote =
        ; close         : string
        ; volume        : string
        ; adjustedClose : string
-}
+    }
+    static member FromJson (_ : HistoricalQuote) =
+            (fun s d o h l c v a -> 
+             {
+               symbol        = s
+               date          = d
+               opening       = o
+               high          = h
+               low           = l
+               close         = c
+               volume        = v
+               adjustedClose = a
+             })
+            <!> Json.read "Symbol"
+            <*> Json.read "Date"
+            <*> Json.read "Open"
+            <*> Json.read "High"
+            <*> Json.read "Low"
+            <*> Json.read "Close"
+            <*> Json.read "Volume"
+            <*> Json.read "Adj_Close"
