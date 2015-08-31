58c1bdc703ce843018dea14d8c66c66231d9d642
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-08-31 10:19:49 +0200

    Added interpolate function

--- src/YFinanceFs/Interpolate.fs
+++ src/YFinanceFs/Interpolate.fs
@@ -6,3 +6,5 @@ let toMarker value =
 let replace (str : string) (marker : string) (replacement : string) =
     System.Text.RegularExpressions.Regex.Replace (str, marker, replacement)
 
+let interpolate input xs =
+    xs |> List.fold (fun s (a, b) -> replace s (toMarker a) b) input
