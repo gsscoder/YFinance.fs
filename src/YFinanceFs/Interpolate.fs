9830727a8829da11f857e0c64c208485bc8e4110
Giacomo Stelluti Scala <gsscoder@gmail.com>
2015-08-31 09:57:20 +0200

    Added basic helpers to Interpolate.fs

--- src/YFinanceFs/Interpolate.fs
+++ src/YFinanceFs/Interpolate.fs
@@ -1,2 +1,8 @@
-﻿module Interpolate
+module Network.Yahoo.Interpolate
+
+let toMarker value =
+    sprintf "#{%s}" value
+
+let replace (str : string) (marker : string) (replacement : string) =
+    System.Text.RegularExpressions.Regex.Replace (str, marker, replacement)
 
